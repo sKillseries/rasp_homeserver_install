54f188a2fb7a1908779777f32aaf747132e0e9fc
sKillseries <48027822+sKillseries@users.noreply.github.com>
2024-03-02 22:47:10 +0100

    Create raspb_install.sh

---
+++ raspb_install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Fonction pour afficher un message d'erreur et quitter le script avec un code d'erreur
+print_error_and_exit() {
+    echo -e"${RED} Erreur: $1" >&2
+    exit 1
+}
+
+# Fonction update
+update() {
+    echo -e "${BLUE} Mise à jour du système en cours... ${NC}"
+    sudo apt-get update || print_error_and_exit "Echec de l'update système"
+    sudo apt-get upgrade -y || print_error_and_exit "Echec de l'upgrade système"
+    echo -e "${GREEN} Fin mise à jour du système."
+}
+
+# Fonction pour installer curl
+install_curl() {
+    echo -e "${BLUE} Installation de curl en cours... ${NC}"
+    sudo apt install -y curl || print_error_and_exit "Échec de l'installation de curl"
+    echo -e "${GREEN} Installation de curl terminée."
+}
+
+# Fonction pour installer Docker
+install_docker() {
+    echo -e "${BLUE} Installation de Docker en cours... ${NC}"
+    curl -fsSL "https://get.docker.com/" -o get-docker.sh || print_error_and_exit "Échec de téléchargement de Docker"
+    sudo sh get-docker.sh || print_error_and_exit "Échec de l'installation de Docker"
+    sudo usermod -aG docker "$(id -u -n)" || print_error_and_exit "Échec de l'ajout de l'utilisateur au groupe docker"
+    newgrp docker || print_error_and_exit "Échec du chargement des nouveaux groupes"
+    echo -e "${GREEN} Installation de Docker terminée."
+}
+
+# Fonction pour installer pip et les modules Python
+install_python_packages() {
+    echo -e "${BLUE} Installation de pip, exegol et automatexpertise en cours... ${NC}"
+    sudo apt install -y python3-pip || print_error_and_exit "Échec de l'installation de pip"
+    python3 -m pip install --upgrade pip || print_error_and_exit "Échec de la mise à jour de pip"
+    python3 -m pip install --user automatexpertise || print_error_and_exit "Échec de l'installation des modules Python"
+    echo -e "${GREEN} Installation des modules Python terminée."
+}
+
+# Fonction pour install Pi-hole
+install_pihole() {
+    echo -e "${BLUE} Installation et configuration de pi-hole en cours... ${NC}"
+    curl https://raw.githubusercontent.com/sKillseries/pi-project/main/docker-compose.yml  -o docker-compose.yml || print_error_and_exit "Echec du téléchargement du dockerfile pihole"
+    sed -i "s/WEBPASSWORD: 'YourPassword'/WEBPASSWORD: '$password'/" ./docker-compose.yml
+    docker compose up  -d || print_error_and_exit "Echec du démarrage du conteneur"
+    echo -e "${GREEN} Installation et configuration terminée."
+}
+
+# Menu d'options
+echo "Choisissez une option :"
+echo "1. Installer docker"
+echo "2. Installer automatexpertise"
+echo "3. Installer pi-hole"
+echo "4. Quitter"
+
+read -p "Votre choix : " choice
+
+case $choice in
+    1)
+        update
+        install_curl
+        install_docker ;;
+    2)
+        install_python_packages ;;
+    3)
+        read -p "Mot de passe désiré pour Pi-Hole: " password
+        install_pihole ;;
+    4)
+        echo "Sortie du script." ;;
+    *)
+        print_error_and_exit "Option invalide." ;;
+esac
+
+echo -e "${YELLOW} Veuillez redémarrer votre système d'exploitation. Sauf si étape installation pi-hole"
+
+exit 0
